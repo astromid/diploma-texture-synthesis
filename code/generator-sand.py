9cc04bc18c8ef283d90a869c0797e039934617a0
Yan <astromid@gmail.com>
2017-05-17 17:03:59 +0300

    Code changes

--- code/generator-sand.py
+++ code/generator-sand.py
@@ -177,11 +177,11 @@ for i in tqdm(range(N_train), desc='Train dataset'):
 
 for i in tqdm(range(N_val), desc='Validation dataset'):
     if l_0 is None:
-        l_start = 30 * uniform()
+        l_start = 35 * uniform()
     else:
         l_start = choice(l_0)
     if l_1 is None:
-        l_end = 30 * uniform()
+        l_end = 35 * uniform()
     else:
         l_end = choice(l_1)
     K = (l_end - l_start) / (W * AA)
